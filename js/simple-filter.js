21542e050872f3c0b11b72fda1993adb11c794d4
Joseph Greene <jgreenemi@gmail.com>
2018-01-28 17:54:17 -0800

    Add a banner image, more robust keyword search.

--- js/simple-filter.js
+++ js/simple-filter.js
@@ -18,3 +18,41 @@ function simpleFilter() {
         updateCount(count);
     });
 };
+
+/** This is a more advanced search, which takes space-delimited keywords and returns entries that have all of them.
+ * A more involved search than the simpleFilter(), this will check that all your space-delimited keyword inputs
+ * are present in the search results.
+ */
+function advancedFilter() {
+    var input, filter, count = 0;
+    
+    // Array of substrings.
+    input = $('#searchbox').val().split(' ');
+
+    // In the #resources_box list of resource entries, hide each that doesn't match the filter.
+    $('#resources_box').find('div').each(function(){
+        // Count up a total, and then later decrement it every time a filter doesn't match. This will ensure
+        // a 0 is displayed when no matches are found.
+        count += 1;
+        var keywordCheckResult = keywordCheck(this.innerHTML.toUpperCase(), input);
+        
+        if (keywordCheckResult){
+            this.style.display = "";
+        } else {
+            count -= 1;
+            this.style.display = "none";
+        }
+        
+        updateCount(count);
+    });
+};
+
+/**
+ * Given two arrays, convert their contents to Uppercase, then return true if all the elements of
+ * subset are found in superset.
+ */
+function keywordCheck(superset, subset){
+    return subset.every(function (value) {
+        return (superset.toUpperCase().indexOf(value.toUpperCase()) >= 0);
+    });
+};
