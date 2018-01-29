c72488f490bb0d40ae5731fec61e471f0ca234a8
Joseph Greene <jgreenemi@gmail.com>
2018-01-28 17:58:09 -0800

    Clean up JS.

--- js/simple-filter.js
+++ js/simple-filter.js
@@ -1,4 +1,6 @@
-// Lifted and modified from W3Schools: https://www.w3schools.com/howto/howto_js_filter_resource_entrysts.asp
+/**
+ * Lifted and modified from W3Schools: https://www.w3schools.com/howto/howto_js_filter_resource_entrysts.asp
+ */
 function simpleFilter() {
     var input, filter, count = 0;
     input = document.getElementById('searchbox');
@@ -19,24 +21,26 @@ function simpleFilter() {
     });
 };
 
-/** This is a more advanced search, which takes space-delimited keywords and returns entries that have all of them.
+/**
  * A more involved search than the simpleFilter(), this will check that all your space-delimited keyword inputs
- * are present in the search results.
+ * are present in the search results. Where simpleFilter() matches exact patterns, this will match out of order
+ * words, and is far more robust.
  */
 function advancedFilter() {
     var input, filter, count = 0;
     
-    // Array of substrings.
+    // Array of substrings, rather than a single string.
     input = $('#searchbox').val().split(' ');
 
     // In the #resources_box list of resource entries, hide each that doesn't match the filter.
     $('#resources_box').find('div').each(function(){
+
         // Count up a total, and then later decrement it every time a filter doesn't match. This will ensure
         // a 0 is displayed when no matches are found.
         count += 1;
-        var keywordCheckResult = keywordCheck(this.innerHTML.toUpperCase(), input);
+        var keywordCheckResult = keywordCheck(this.innerHTML, input);
         
-        if (keywordCheckResult){
+        if (keywordCheckResult) {
             this.style.display = "";
         } else {
             count -= 1;
@@ -49,7 +53,7 @@ function advancedFilter() {
 
 /**
  * Given two arrays, convert their contents to Uppercase, then return true if all the elements of
- * subset are found in superset.
+ * subset are found in superset, else return false.
  */
 function keywordCheck(superset, subset){
     return subset.every(function (value) {
