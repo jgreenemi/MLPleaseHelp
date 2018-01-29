6027058727f28e0fd095409b6eeaa48c1066586a
Joseph Greene <jgreenemi@gmail.com>
2018-01-28 20:16:46 -0800

    Undo loop logic change as this broke Chrome compatibility.

--- js/ml-display.js
+++ js/ml-display.js
@@ -12,15 +12,16 @@ $(document).ready(function(){
 
 function retrieveDictionary(){
     $.getScript('js/ml-assets.js', function(){
-        for (var mlAsset in mlAssets) {
+    console.log(mlAssets);
+        for (var mlAsset of mlAssets) {
             $('#resources_box').append(
                 $('<div/>')
                     .attr('id', mlAsset['Name'])
                     .addClass('resource_entry')
                     .append(
                         '<a href=\"' +
-                        mlAsset['Link'] + '\">' + 
-                        mlAsset['Name'] + '</a>: ' + 
+                        mlAsset['Link'] + '\">' +
+                        mlAsset['Name'] + '</a>: ' +
                         mlAsset['Description'] +
                         '<span class="keywords"> ' +
                         mlAsset['Keywords'] +
