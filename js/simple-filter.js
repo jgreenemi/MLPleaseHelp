5e91e2e6e5a99fd8a238f1ec7fecc8b6686e3998
Joseph Greene <jgreenemi@gmail.com>
2018-01-28 16:37:10 -0800

    Clean up simpleFilter().

--- js/simple-filter.js
+++ js/simple-filter.js
@@ -1,12 +1,10 @@
-// resource_entryfted from W3Schools: https://www.w3schools.com/howto/howto_js_filter_resource_entrysts.asp
+// Lifted and modified from W3Schools: https://www.w3schools.com/howto/howto_js_filter_resource_entrysts.asp
 function simpleFilter() {
-    var input, filter, resources_box, resource_entry, span, i;
+    var input, filter;
     input = document.getElementById('searchbox');
     filter = input.value.toUpperCase();
-    resources_box = document.getElementById("resources_box");
-    resource_entry = resources_box.getElementsByTagName('div');
 
-    // Gonna try to replace the for (i++) logic with for var of resources_box.
+    // In the #resources_box list of resource entries, hide each that doesn't match the filter.
     $('#resources_box').find('div').each(function(){
         if (this.innerHTML.toUpperCase().indexOf(filter) > -1) {
             this.style.display = "";
@@ -14,14 +12,4 @@ function simpleFilter() {
             this.style.display = "none";
         }
     });
-    
-    // Loop through all resource_entryst items, and hide those who don't match the search query
-    // for (i = 0; i < resource_entry.length; i++) {
-    //     span = resource_entry[i].getElementsByTagName("span")[0];
-    //     if (span.innerHTML.toUpperCase().indexOf(filter) > -1) {
-    //         resource_entry[i].style.display = "";
-    //     } else {
-    //         resource_entry[i].style.display = "none";
-    //     }
-    // }
 };
