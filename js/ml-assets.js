5a946d37653a1af59b9ec978108e51c0349e5083
Joseph Greene <jgreenemi@gmail.com>
2018-01-29 23:49:53 -0800

    Add guidance in the instructions, reorder existing entries based on when they were contributed.

--- js/ml-assets.js
+++ js/ml-assets.js
@@ -1,4 +1,4 @@
-/* To add new resource entries, make a new dict-like entry like the one below:
+/* To add new resource entries, make a new dict-like entry at the top of the mlAssets array, like the one below:
 {
     "Name": "Parris",
     "Description": "Parris, the automated infrastructure setup tool for machine learning algorithms.",
@@ -8,14 +8,24 @@
 },
 
 Note:
+- Add new entries to the top of the array, that way the newest entries show up at the top of the page.
 - LastUpdatedDate does not get used in the engine, just helps to keep track of when entries were last changed.
 - Keywords is a string, not a list. Commas are encouraged for readability but not required.
-- Please only link to the original repository or the resource's original website, not a blog post or other middle-man page.
-- Entries are displayed in the order they were added to the list. In the future I may introduce logic to do some other sorting (i.e. by LastUpdatedDate) but this will depend on what the community finds useful.
+- Only link to the original repository or the resource's original website, not a blog post or other middle-man
+  page, unless that post or article *is* the resource to be shared.
+- Entries are displayed in the order they were added to the list. In the future I may introduce logic to do some other
+  sorting (i.e. by LastUpdatedDate) but this will depend on what the community finds useful.
 */
 
 
 var mlAssets = [
+{
+    "Name": "Neural Networks and Deep Learning",
+    "Description": "Neural Networks and Deep Learning is a free online book. The book will teach you about:  Neural networks, a beautiful biologically-inspired programming paradigm which enables a computer to learn from observational data, and Deep learning, a powerful set of techniques for learning in neural networks",
+    "Link": "http://neuralnetworksanddeeplearning.com/",
+    "Keywords": "Neural Networks, Deep Learning",
+    "LastUpdatedDate": "2018/1/29",
+},
 {
     "Name": "Roadmap to Machine Learning",
     "Description": "A complete, open source roadmap designed to take you from zero knowledge up to machine learning expertise. It includes resources such as ML and relevant math/programming courses, books and public datasets.",
@@ -24,45 +34,10 @@ var mlAssets = [
     "LastUpdatedDate": "2018/01/29",
 },
 {
-    "Name": "Parris",
-    "Description": "Parris, the automated infrastructure setup tool for machine learning algorithms.",
-    "Link": "https://github.com/jgreenemi/Parris",
-    "Keywords": "Python, AWS",
-    "LastUpdatedDate": "2018/01/28",
-},
-{
-    "Name": "NapkinML",
-    "Description": "A tiny lib with pocket-sized implementations of machine learning models in NumPy.",
-    "Link": "https://github.com/eriklindernoren/NapkinML",
-    "Keywords": "Python",
-    "LastUpdatedDate": "2018/01/28",
-},
-{
-    "Name": "Keras",
-    "Description": "Deep Learning for humans http://keras.io/",
-    "Link": "https://github.com/keras-team/keras",
-    "Keywords": "Python, Library",
-    "LastUpdatedDate": "2018/01/28",
-},
-{
-    "Name": "Nvidia-docker",
-    "Description": "Build and run Docker containers leveraging NVIDIA GPUs",
-    "Link": "https://github.com/NVIDIA/nvidia-docker",
-    "Keywords": "Makefile, Shell",
-    "LastUpdatedDate": "2018/01/28",
-},
-{
-    "Name": "evolution-strategies-starter",
-    "Description": "This is a distributed implementation of the algorithm described in Evolution Strategies as a Scalable Alternative to Reinforcement Learning (Tim Salimans, Jonathan Ho, Xi Chen, Ilya Sutskever).",
-    "Link": "https://github.com/openai/evolution-strategies-starter",
-    "Keywords": "Python",
-    "LastUpdatedDate": "2018/01/28",
-},
-{
-    "Name": "Conv Nets: A Modular Perspective",
-    "Description": "This post is part of a series on convolutional neural networks and their generalizations.",
-    "Link": "http://colah.github.io/posts/2014-07-Conv-Nets-Modular/",
-    "Keywords": "Blog, CNN, Reading",
+    "Name": "Stanford CS109 Course Reader",
+    "Description": "Probability for Computer Scientists From Counting to Deep Learning",
+    "Link": "http://web.stanford.edu/class/cs109/courseReader.html",
+    "Keywords": "Class, Education, Blog, Reading",
     "LastUpdatedDate": "2018/01/28",
 },
 {
@@ -73,17 +48,45 @@ var mlAssets = [
     "LastUpdatedDate": "2018/01/28",
 },
 {
-    "Name": "Stanford CS109 Course Reader",
-    "Description": "Probability for Computer Scientists From Counting to Deep Learning",
-    "Link": "http://web.stanford.edu/class/cs109/courseReader.html",
-    "Keywords": "Class, Education, Blog, Reading",
+    "Name": "Conv Nets: A Modular Perspective",
+    "Description": "This post is part of a series on convolutional neural networks and their generalizations.",
+    "Link": "http://colah.github.io/posts/2014-07-Conv-Nets-Modular/",
+    "Keywords": "Blog, CNN, Reading",
     "LastUpdatedDate": "2018/01/28",
 },
 {
-    "Name": "Neural Networks and Deep Learning",
-    "Description": "Neural Networks and Deep Learning is a free online book. The book will teach you about:  Neural networks, a beautiful biologically-inspired programming paradigm which enables a computer to learn from observational data, and Deep learning, a powerful set of techniques for learning in neural networks",
-    "Link": "http://neuralnetworksanddeeplearning.com/",
-    "Keywords": "Neural Networks, Deep Learning",
-    "LastUpdatedDate": "2018/1/29",
+    "Name": "evolution-strategies-starter",
+    "Description": "This is a distributed implementation of the algorithm described in Evolution Strategies as a Scalable Alternative to Reinforcement Learning (Tim Salimans, Jonathan Ho, Xi Chen, Ilya Sutskever).",
+    "Link": "https://github.com/openai/evolution-strategies-starter",
+    "Keywords": "Python",
+    "LastUpdatedDate": "2018/01/28",
+},
+{
+    "Name": "Nvidia-docker",
+    "Description": "Build and run Docker containers leveraging NVIDIA GPUs",
+    "Link": "https://github.com/NVIDIA/nvidia-docker",
+    "Keywords": "Makefile, Shell",
+    "LastUpdatedDate": "2018/01/28",
+},
+{
+    "Name": "Keras",
+    "Description": "Deep Learning for humans http://keras.io/",
+    "Link": "https://github.com/keras-team/keras",
+    "Keywords": "Python, Library",
+    "LastUpdatedDate": "2018/01/28",
+},
+{
+    "Name": "NapkinML",
+    "Description": "A tiny lib with pocket-sized implementations of machine learning models in NumPy.",
+    "Link": "https://github.com/eriklindernoren/NapkinML",
+    "Keywords": "Python",
+    "LastUpdatedDate": "2018/01/28",
+},
+{
+    "Name": "Parris",
+    "Description": "Parris, the automated infrastructure setup tool for machine learning algorithms.",
+    "Link": "https://github.com/jgreenemi/Parris",
+    "Keywords": "Python, AWS",
+    "LastUpdatedDate": "2018/01/28",
 },
 ]
