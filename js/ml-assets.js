31d58af00a1434829fe03af42740600d762139d9
greed2411 <jaiimmortal@gmail.com>
2018-01-31 11:38:34 +0530

    Revert "reverting"
    This reverts commit 4a0f0e36362f2fbd71f96681d1b7b65283a080cb, reversing
    changes made to 5c7418853a78ee452355a01290b9aebc1bd44f56.

--- js/ml-assets.js
+++ js/ml-assets.js
@@ -1,4 +1,4 @@
-/* To add new resource entries, make a new dict-like entry at the top of the mlAssets array, like the one below:
+/* To add new resource entries, make a new dict-like entry like the one below:
 {
     "Name": "Parris",
     "Description": "Parris, the automated infrastructure setup tool for machine learning algorithms.",
@@ -8,92 +8,19 @@
 },
 
 Note:
-- Add new entries to the top of the array, that way the newest entries show up at the top of the page.
 - LastUpdatedDate does not get used in the engine, just helps to keep track of when entries were last changed.
 - Keywords is a string, not a list. Commas are encouraged for readability but not required.
-- Only link to the original repository or the resource's original website, not a blog post or other middle-man
-  page, unless that post or article *is* the resource to be shared.
-- Entries are displayed in the order they were added to the list. In the future I may introduce logic to do some other
-  sorting (i.e. by LastUpdatedDate) but this will depend on what the community finds useful.
+- Please only link to the original repository or the resource's original website, not a blog post or other middle-man page.
+- Entries are displayed in the order they were added to the list. In the future I may introduce logic to do some other sorting (i.e. by LastUpdatedDate) but this will depend on what the community finds useful.
 */
 
 
 var mlAssets = [
 {
-    "Name": "Introduction to Applied Linear Algebra – Vectors, Matrices, and Least Squares",
-    "Description": "Book on Linear Algebra from Stephen Boyd and Lieven Vandenberghe",
-    "Link": "https://web.stanford.edu/%7Eboyd/vmls/",
-    "Keywords": "book, reading, math",
-    "LastUpdatedDate": "2018/1/30",
-},
-{
-    "Name": "cnn-text-classification-tf: Implementing a CNN for Text Classification in TensorFlow",
-    "Description": "Convolutional Neural Network for Text Classification in Tensorflow by Denny Britz",
-    "Link": "https://github.com/dennybritz/cnn-text-classification-tf",
-    "Keywords": "library, examples, tf, cnn, reading, blog",
-    "LastUpdatedDate": "2018/1/30",
-},
-{
-    "Name": "deeplearning.ai",
-    "Description": "Courses served via Coursera with a focus in deep learning, provided by noted machine learning professor Andrew Ng.",
-    "Link": "https://www.deeplearning.ai/",
-    "Keywords": "deep learning, neural networks, courses, classes, education, coursera",
-    "LastUpdatedDate": "2018/1/30",
-},
-{
-    "Name": "Neural Networks and Deep Learning",
-    "Description": "Neural Networks and Deep Learning is a free online book. The book will teach you about:  Neural networks, a beautiful biologically-inspired programming paradigm which enables a computer to learn from observational data, and Deep learning, a powerful set of techniques for learning in neural networks",
-    "Link": "http://neuralnetworksanddeeplearning.com/",
-    "Keywords": "Neural Networks, Deep Learning",
-    "LastUpdatedDate": "2018/1/29",
-},
-{
-    "Name": "Roadmap to Machine Learning",
-    "Description": "A complete, open source roadmap designed to take you from zero knowledge up to machine learning expertise. It includes resources such as ML and relevant math/programming courses, books and public datasets.",
-    "Link": "https://howicodestuff.github.io/machine_learning/2018/01/12/a-roadmap-to-machine-learning.html",
-    "Keywords": "Roadmap, Python, Blog, Education, Reading",
-    "LastUpdatedDate": "2018/01/29",
-},
-{
-    "Name": "Stanford CS109 Course Reader",
-    "Description": "Probability for Computer Scientists From Counting to Deep Learning",
-    "Link": "http://web.stanford.edu/class/cs109/courseReader.html",
-    "Keywords": "Class, Education, Blog, Reading",
-    "LastUpdatedDate": "2018/01/28",
-},
-{
-    "Name": "MIT 6.S099: Artificial General Intelligence",
-    "Description": "This class takes an engineering approach to exploring possible research paths toward building human-level intelligence. The lectures will introduce our current understanding of computational intelligence and ways in which strong AI could possibly be achieved, with insights from deep learning, reinforcement learning, computational neuroscience, robotics, cognitive modeling, psychology, and more.",
-    "Link": "https://agi.mit.edu/",
-    "Keywords": "Class, Education, Reading",
-    "LastUpdatedDate": "2018/01/28",
-},
-{
-    "Name": "Conv Nets: A Modular Perspective",
-    "Description": "This post is part of a series on convolutional neural networks and their generalizations.",
-    "Link": "http://colah.github.io/posts/2014-07-Conv-Nets-Modular/",
-    "Keywords": "Blog, CNN, Reading",
-    "LastUpdatedDate": "2018/01/28",
-},
-{
-    "Name": "evolution-strategies-starter",
-    "Description": "This is a distributed implementation of the algorithm described in Evolution Strategies as a Scalable Alternative to Reinforcement Learning (Tim Salimans, Jonathan Ho, Xi Chen, Ilya Sutskever).",
-    "Link": "https://github.com/openai/evolution-strategies-starter",
-    "Keywords": "Python",
-    "LastUpdatedDate": "2018/01/28",
-},
-{
-    "Name": "Nvidia-docker",
-    "Description": "Build and run Docker containers leveraging NVIDIA GPUs",
-    "Link": "https://github.com/NVIDIA/nvidia-docker",
-    "Keywords": "Makefile, Shell",
-    "LastUpdatedDate": "2018/01/28",
-},
-{
-    "Name": "Keras",
-    "Description": "Deep Learning for humans http://keras.io/",
-    "Link": "https://github.com/keras-team/keras",
-    "Keywords": "Python, Library",
+    "Name": "Parris",
+    "Description": "Parris, the automated infrastructure setup tool for machine learning algorithms.",
+    "Link": "https://github.com/jgreenemi/Parris",
+    "Keywords": "Python, AWS",
     "LastUpdatedDate": "2018/01/28",
 },
 {
@@ -104,13 +31,47 @@ var mlAssets = [
     "LastUpdatedDate": "2018/01/28",
 },
 {
-    "Name": "Parris",
-    "Description": "Parris, the automated infrastructure setup tool for machine learning algorithms.",
-    "Link": "https://github.com/jgreenemi/Parris",
-    "Keywords": "Python, AWS",
+    "Name": "Keras",
+    "Description": "Deep Learning for humans http://keras.io/",
+    "Link": "https://github.com/keras-team/keras",
+    "Keywords": "Python, Library",
     "LastUpdatedDate": "2018/01/28",
 },
-<<<<<<< HEAD
+{
+    "Name": "Nvidia-docker",
+    "Description": "Build and run Docker containers leveraging NVIDIA GPUs",
+    "Link": "https://github.com/NVIDIA/nvidia-docker",
+    "Keywords": "Makefile, Shell",
+    "LastUpdatedDate": "2018/01/28",
+},
+{
+    "Name": "evolution-strategies-starter",
+    "Description": "This is a distributed implementation of the algorithm described in Evolution Strategies as a Scalable Alternative to Reinforcement Learning (Tim Salimans, Jonathan Ho, Xi Chen, Ilya Sutskever).",
+    "Link": "https://github.com/openai/evolution-strategies-starter",
+    "Keywords": "Python",
+    "LastUpdatedDate": "2018/01/28",
+},
+{
+    "Name": "Conv Nets: A Modular Perspective",
+    "Description": "This post is part of a series on convolutional neural networks and their generalizations.",
+    "Link": "http://colah.github.io/posts/2014-07-Conv-Nets-Modular/",
+    "Keywords": "Blog, CNN, Reading",
+    "LastUpdatedDate": "2018/01/28",
+},
+{
+    "Name": "MIT 6.S099: Artificial General Intelligence",
+    "Description": "This class takes an engineering approach to exploring possible research paths toward building human-level intelligence. The lectures will introduce our current understanding of computational intelligence and ways in which strong AI could possibly be achieved, with insights from deep learning, reinforcement learning, computational neuroscience, robotics, cognitive modeling, psychology, and more.",
+    "Link": "https://agi.mit.edu/",
+    "Keywords": "Class, Education, Reading",
+    "LastUpdatedDate": "2018/01/28",
+},
+{
+    "Name": "Stanford CS109 Course Reader",
+    "Description": "Probability for Computer Scientists From Counting to Deep Learning",
+    "Link": "http://web.stanford.edu/class/cs109/courseReader.html",
+    "Keywords": "Class, Education, Blog, Reading",
+    "LastUpdatedDate": "2018/01/28",
+},
 {
     "Name": "Books for reading",
     "Description": "Important books for Machine Learning and DBMS and programming languages",
@@ -175,6 +136,3 @@ var mlAssets = [
     "LastUpdatedDate": "2018/01/30",
 },
 ]
-=======
-]
->>>>>>> upstream/master
